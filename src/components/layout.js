dd3e43622897f0d57494d24d20198f988043f203
Aaron Cox <aaron@greymass.com>
2019-08-26 23:21:38 -0400

    Added i18next provider

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react"
 import { css } from "@emotion/core";
+
+import { I18nextProvider } from 'react-i18next';
+
+import i18n from '../utils/i18n';
+
 import { Link } from "gatsby";
 import { Helmet } from "react-helmet";
 import { Segment, Button } from 'semantic-ui-react';
@@ -14,66 +19,68 @@ class Layout extends Component {
     const { children, t } = this.props;
 
     return (
-      <Segment
-        basic
-      >
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title>Greymass</title>
-          <link rel="canonical" href="http://mysite.com/example" />
-        </Helmet>
-        <Link to={`/`}>
-          <h3
-            css={css`
-            margin-bottom: ${rhythm(2)};
-            display: inline-block;
-            font-style: normal;
-          `}
-          >
-            {t('home')}
-          </h3>
-        </Link>
-        &nbsp;
-        |
-        &nbsp;
-        <Link
-          to={`/about/`}
-        >
-          <h3
-            css={css`
-            margin-bottom: ${rhythm(2)};
-            display: inline-block;
-            font-style: normal;
-          `}
-          >
-            {t('about')}
-          </h3>
-        </Link>
-
-        <br />
-        <h2>{t('heading')}</h2>
+      <I18nextProvider i18n={i18n}>
         <Segment
-          css={css`
+          basic
+        >
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>Greymass</title>
+            <link rel="canonical" href="http://mysite.com/example" />
+          </Helmet>
+          <Link to={`/`}>
+            <h3
+              css={css`
+            margin-bottom: ${rhythm(2)};
+            display: inline-block;
+            font-style: normal;
+          `}
+            >
+              {t('home')}
+            </h3>
+          </Link>
+          &nbsp;
+          |
+          &nbsp;
+          <Link
+            to={`/about/`}
+          >
+            <h3
+              css={css`
+            margin-bottom: ${rhythm(2)};
+            display: inline-block;
+            font-style: normal;
+          `}
+            >
+              {t('about')}
+            </h3>
+          </Link>
+
+          <br />
+          <h2>{t('heading')}</h2>
+          <Segment
+            css={css`
             margin: 50 auto;
             max-width: 700px;
             padding: ${rhythm(10)};
             padding-top: ${rhythm(1.5)};
           `}
-        >
-          {children}
+          >
+            {children}
+          </Segment>
+          <br />
+          <br />
+          Semantic UI Example:
+          <br />
+          <Button
+            content="This button is useless!"
+          />
+          <br />
+          <br />
+          Switch between Languages:
+          <LanguageSwitcher />
         </Segment>
-        <br />
-        <br />
-        Semantic UI Example:
-        <br />
-        <Button
-          content="This button is useless!"
-        />
-        <br />
-        <br />
-        Switch between Languages:
-        <LanguageSwitcher />
-      </Segment>
+      </I18nextProvider>
     )
   }
 }
