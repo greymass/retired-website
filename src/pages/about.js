a4fe75eeb795a4786846585b16b6e34c56882f7c
Aaron Cox <aaron@greymass.com>
2019-08-27 12:32:04 -0400

    started localizing about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,20 +1,32 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { translate } from 'react-i18next';
 
 import Layout from '../components/layout';
 
-export default ({ data }) => (
-  <Layout>
-    { () => (
-      <React.Fragment>
-        <h1>About {data.site.siteMetadata.title}</h1>
-        <p>
-          We are Greymass!
-        </p>
-      </React.Fragment>
-    )}
-  </Layout>
-)
+class About extends React.Component {
+  render() {
+    const {
+      data,
+      t
+    } = this.props;
+    return (
+      <Layout>
+        { () => (
+          <React.Fragment>
+            <p>Foo: {t('foo')}</p>
+            <h1>About {data.site.siteMetadata.title}</h1>
+            <p>
+              We are Greymass!
+            </p>
+          </React.Fragment>
+        )}
+      </Layout>
+    )
+  }
+}
+
+export default translate('about')(About);
 
 export const query = graphql`
   query {
