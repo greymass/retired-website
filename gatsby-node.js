fd77820c749e96ee4cef3ea6fc42a0d16e89d187
dafuga <danielfugere28@gmail.com>
2019-08-28 13:10:12 +0900

    enhancement: reloading locales on file change

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,52 +2,23 @@ const path = require(`path`)
 const fs = require("fs-extra")
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
-// exports.onCreateNode = ({ node, getNode, actions }) => {
-//   const { createNodeField } = actions
-//   if (node.internal.type === `MarkdownRemark`) {
-//     const slug = createFilePath({ node, getNode, basePath: `pages` })
-//     createNodeField({
-//       node,
-//       name: `slug`,
-//       value: slug,
-//     })
-//   }
-// }
-
-// exports.createPages = async ({ graphql, actions }) => {
-//   const { createPage } = actions
-//   const result = await graphql(`
-//     {
-//       allMarkdownRemark {
-//         edges {
-//           node {
-//             fields {
-//               slug
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `)
-//
-//   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-//     createPage({
-//       path: node.fields.slug,
-//       component: path.resolve(`./src/templates/blog-post.js`),
-//       context: {
-//         // Data passed to context is available
-//         // in page queries as GraphQL variables.
-//         slug: node.fields.slug,
-//       },
-//     })
-//   })
-// }
-
 // To get locale folder loaded
+
 exports.onPostBuild = () => {
-  console.log("Copying locales")
+  console.log("Copying locales for build")
   fs.copySync(
     path.join(__dirname, "/src/locales"),
     path.join(__dirname, "/public/locales")
   )
+}
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  if (node.dir && node.dir.includes('locales/')) {
+    console.log("Copying locales because of file change")
+
+    fs.copySync(
+      path.join(__dirname, "/src/locales"),
+      path.join(__dirname, "/public/locales")
+    )
+  }
 }
