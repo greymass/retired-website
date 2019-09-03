970bca683ca042f5f8e8c3684c42ab6db8cba1de
dafuga <danielfugere28@gmail.com>
2019-09-03 22:13:19 +0900

    chore: polished the footer components

--- src/components/layout/footer/info.js
+++ src/components/layout/footer/info.js
@@ -1,41 +1,36 @@
-import React, { Component } from "react";
-import { Grid } from "semantic-ui-react";
-import { graphql, Link, StaticQuery } from "gatsby"
-import { translate } from "react-i18next"
+import React, { Component } from 'react';
+import { Grid } from 'semantic-ui-react';
+import { graphql, Link, StaticQuery } from 'gatsby';
+import { translate } from 'react-i18next';
+
+import FooterInfoLink from './info/link';
 
 class FooterInfo extends Component {
   render() {
     const { data, t } = this.props;
 
+    const containerStyle={
+      backgroundColor: '#343941'
+    }
+
     return (
-      <div>
-        <Grid>
-          <Grid.Column width={6}>
-            <Link to="#bp-info" style={{ margin: 5 }}>
-              {t('bp_info')}
-            </Link>
-            <Link to="#code-of-conduct" style={{ margin: 5 }}>
-              {t('code_of_conduct')}
-            </Link>
-            <Link to="#ownership" style={{ margin: 5 }}>
-              {t('ownership')}
-            </Link>
-            <Link to="#terms-of-use" style={{ margin: 5 }}>
-              {t('terms_of_use')}
-            </Link>
-            <Link to="#privacy-policy" style={{ margin: 5 }}>
-              {t('privacy_policy')}
-            </Link>
-            <h4>
-              {t('copyright_text')}
-            </h4>
-          </Grid.Column>
-          <Grid.Column width={10}>
-            <h4>{data.site.siteMetadata.officialEmail}</h4>
-            <h4>{data.site.siteMetadata.officialAddress}</h4>
-          </Grid.Column>
-        </Grid>
-      </div>
+      <Grid style={containerStyle}>
+        <Grid.Column width={6}>
+          <FooterInfoLink to={`bp-info`} content={t('bp_info')} />
+          <FooterInfoLink to={`code-of-conduct`} content={t('code_of_conduct')} />
+          <FooterInfoLink to={`ownership`} content={t('ownership')} />
+          <FooterInfoLink to={`terms-of-use`} content={t('terms_of_use')} />
+          <FooterInfoLink to={`privacy-policy`} content={t('privacy_policy')} />
+
+          <h4>
+            {t('copyright_text')}
+          </h4>
+        </Grid.Column>
+        <Grid.Column width={10}>
+          <h4>{data.site.siteMetadata.officialEmail}</h4>
+          <h4>{data.site.siteMetadata.officialAddress}</h4>
+        </Grid.Column>
+      </Grid>
     )
   }
 }
