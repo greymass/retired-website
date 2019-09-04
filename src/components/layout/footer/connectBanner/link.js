b2d04c5ad2af1660358af01d42eb34387f3e2feb
dafuga <danielfugere28@gmail.com>
2019-09-04 23:08:00 +0900

    chore: finish polishing the footer components

--- src/components/layout/footer/connectBanner/link.js
+++ src/components/layout/footer/connectBanner/link.js
@@ -1,26 +1,44 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import { Link } from "gatsby";
+import { Link } from 'gatsby';
+import { Icon } from 'semantic-ui-react'
 
 class LayoutHeaderLink extends Component {
+  state = {
+    active: false
+  };
+
   render() {
     const {
-      active,
       type,
       to,
     } = this.props;
 
+    const { active } = this.state;
+
     const containerStyles = {
-      padding: active ? '13px' : '10px',
-      paddingBottom: '0px',
+      background: active ? '#0091E2' : 'white',
+      border: active ? '6px solid white' : '',
+      borderRadius: '50%',
+      margin: '10px',
+      padding: '30px 12px 18px 16px',
     };
 
     return (
-      <div style={containerStyles}>
-        <Link to={to}>
-          {type}
-        </Link>
-      </div>
+      <Link
+        to={to}
+        style={containerStyles}
+        onMouseEnter={() => this.setState({ active: true})}
+        onMouseLeave={() => this.setState({ active: false})}
+      >
+        <Icon
+          name={type}
+          style={{
+            fontSize: '30px',
+            color: active ? 'white' : '#343941'
+          }}
+        />
+      </Link>
     )
   }
 }
