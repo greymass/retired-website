5bc2565f713420498972bbd10dad89cf66543b32
dafuga <danielfugere28@gmail.com>
2019-09-12 16:40:24 +0900

    chore: changed the home components to use css modules

--- src/components/home/header.js
+++ src/components/home/header.js
@@ -1,78 +1,42 @@
 import React, { Component } from 'react';
 
-import { Container } from 'semantic-ui-react';
 import { translate } from 'react-i18next';
 import { Link } from 'gatsby';
 
 import greymassHeaderBackground from '../../images/greymassHeaderBackground.svg';
+import homeHeaderStyles from './header.module.css';
 
 class HomeHeader extends Component {
   render() {
     const { t } = this.props;
 
-    const backgroundStyles = {
-      display: 'block',
-      margin: 'auto',
-      maxWidth: '800px',
-    };
-    const headerTextStyles = {
-      fontFamily: 'Montserrat',
-      fontSize: '26px',
-      fontStyle: 'normal',
-      fontWeight: '500',
-      letterSpacing: '0.02em',
-      lineHeight: '44px',
-      margin: 'auto',
-      paddingTop: '50px',
-      paddingBottom: '60px',
-      maxWidth: '600px',
-      textAlign: 'center',
-      textTransform: 'uppercase',
-    };
-    const subheaderTextStyles = {
-      fontFamily: 'Roboto',
-      fontSize: '18px',
-      fontStyle: 'normal',
-      fontWeight: 'normal',
-      letterSpacing: '0.02em',
-      lineHeight: '30px',
-      textAlign: 'center',
-      margin: '0',
-    };
-    const supportUsLinkStyle = {
-      color: '#0091E2',
-      textTransform: 'uppercase',
-      fontSize: '16px',
-      marginLeft: '5px',
-    };
-
     return (
-      <Container style={{ paddingBottom: '50px' }} basic>
-        <h4 style={headerTextStyles}>
+      <div style={{ paddingBottom: '50px' }} basic>
+        <h4 className={homeHeaderStyles.headerText}>
          {t('header_one')}
         </h4>
         <img
           alt='greymass-header'
-          style={backgroundStyles}
+          className={homeHeaderStyles.background}
           src={greymassHeaderBackground}
         />
-        <h4 style={headerTextStyles}>
+        <h4 className={homeHeaderStyles.headerText}>
           {t('header_two')}
         </h4>
 
         <div style={{ marginTop: '40px' }}>
-          <h5 style={subheaderTextStyles}>
+          <h5 className={homeHeaderStyles.subheaderText}>
             {t('header_bottom_one')}
           </h5>
-          <h5 style={subheaderTextStyles}>
+          <h5 className={homeHeaderStyles.subheaderText}>
             {t('header_bottom_two')}
 
-            <Link style={supportUsLinkStyle} to={`#support-us`}>
+            <Link className={homeHeaderStyles.supportUsLink} to={`#support-us`}>
               {t('header_bottom_link')}
             </Link>
           </h5>
         </div>
-      </Container>
+      </div>
     )
   }
 }
--- src/components/home/newsletter.js
+++ src/components/home/newsletter.js
@@ -3,45 +3,18 @@ import React, { Component } from "react"
 import { Container } from 'semantic-ui-react';
 import { translate } from 'react-i18next';
 
+import newsletterStyles from './newsletter.module.css';
+
 class HomeNewsletter extends Component {
   render() {
     const { t } = this.props;
 
-    const containerStyles = {
-      padding: '60px',
-      minHeight: '500px'
-    }
-
-    const titleStyles = {
-      fontFamily: 'Montserrat',
-      fontSize: '26px',
-      fontStyle: 'normal',
-      fontWeight: '500',
-      letterSpacing: '0.02em',
-      lineHeight: '44px',
-      margin: 'auto',
-      marginBottom: '50px',
-      maxWidth: '600px',
-      textAlign: 'center',
-      textTransform: 'uppercase',
-    };
-    const subtitleStyles = {
-      fontFamily: 'Roboto',
-      fontSize: '18px',
-      fontStyle: 'normal',
-      fontWeight: 'normal',
-      letterSpacing: '0.02em',
-      lineHeight: '30px',
-      textAlign: 'center',
-      margin: '0',
-    };
-
     return (
-      <Container style={containerStyles} basic>
-        <h4 style={titleStyles}>
+      <Container className={newsletterStyles.container} basic>
+        <h4 className={newsletterStyles.title}>
           {t('newsletter_title')}
         </h4>
-        <h4 style={subtitleStyles}>
+        <h4 className={newsletterStyles.subtitle}>
           {t('newsletter_subtitle')}
         </h4>
       </Container>
