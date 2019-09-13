b66b5f8fad2c96e17cf72cc53c146a1b029c69e5
dafuga <danielfugere28@gmail.com>
2019-09-13 18:02:31 +0900

    fix: setting relative path from root

--- src/components/about/teamMembers.js
+++ src/components/about/teamMembers.js
@@ -76,18 +76,18 @@ export default props => (
               }
             }
           }
-          profileImages: allFile(filter: {relativeDirectory: {eq: "images/teamMembers"}}) {
+          profileImages: allFile(filter: {relativeDirectory: {regex: "/teamMembers/"}, extension: {regex: "/(jpg)|(jpeg)|(png)/"}}) {
             edges {
-            node {
-              childImageSharp {
-                fluid(maxWidth: 600) {
-                  ...GatsbyImageSharpFluid
+              node {
+                childImageSharp {
+                  fluid(maxWidth: 600) {
+                    ...GatsbyImageSharpFluid
+                  }
                 }
               }
             }
           }
-        }
-      }
+       }
     `}
     render={data => <AboutTeamMembersWrapper data={data} {...props} />}
   />
