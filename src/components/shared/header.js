042af9ed47f8276ab00e846f2dfb2eca5aa3e05a
dafuga <danielfugere28@gmail.com>
2019-09-17 22:25:53 +0900

    refactor: moved header for about and projects to shared component

---
+++ src/components/shared/header.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react"
+
+import { Grid } from 'semantic-ui-react';
+
+import sharedHeaderStyles from './header.module.css';
+
+export default class SharedHeader extends Component {
+  render() {
+    const { title, paragraph } = this.props;
+
+    return (
+      <Grid container className={sharedHeaderStyles.container} basic>
+        <Grid.Column>
+          <div className={sharedHeaderStyles.blueBar} />
+        </Grid.Column>
+        <Grid.Column width={10} >
+          <h4 className={sharedHeaderStyles.headerText}>
+            {title}
+          </h4>
+          <p className={sharedHeaderStyles.paragraphText}>
+            {paragraph}
+          </p>
+        </Grid.Column>
+      </Grid>
+    )
+  }
+}
