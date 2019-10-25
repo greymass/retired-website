111a4f4612d0521e253e2d671e879aacff3eb662
Aaron Cox <aaron@greymass.com>
2019-10-25 03:32:48 -0400

    Reworking Gatsby for the intl plugin

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,27 +2,6 @@ const path = require(`path`)
 const fs = require("fs-extra")
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
-// To get locale folder loaded
-
-exports.onPostBuild = () => {
-  console.log("Copying locales for build")
-  fs.copySync(
-    path.join(__dirname, "/src/locales"),
-    path.join(__dirname, "/public/locales")
-  )
-}
-
-exports.onCreateNode = ({ node, getNode, actions }) => {
-  if (node.dir && node.dir.includes('locales/')) {
-    console.log("Copying locales because of file change")
-
-    fs.copySync(
-      path.join(__dirname, "/src/locales"),
-      path.join(__dirname, "/public/locales")
-    )
-  }
-}
-
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
