f2eb573661511f74840d99bbad4fc042b8595aaa
Aaron Cox <aaron@greymass.com>
2019-10-25 03:38:47 -0400

    disabled language switcher

--- src/components/layout/footer/info/contact/languageSwitcher.js
+++ src/components/layout/footer/info/contact/languageSwitcher.js
@@ -1,39 +1,37 @@
 import React, { Component } from 'react'
-import { translate } from 'react-i18next'
 import { Dropdown, Menu } from "semantic-ui-react"
-import { navigate } from 'gatsby'
+// import { navigate } from 'gatsby'
 
 class FooterInfoLanguageSwitcher extends Component {
-  constructor(props) {
-    super(props)
-
-    const { i18n } = this.props;
-
-    this.state = { language: i18n.language }
-  }
+  // constructor(props) {
+  //   super(props)
+  //
+  //   const { i18n } = this.props;
+  //
+  //   this.state = { language: i18n.language }
+  // }
 
   handleLanguageChange = (e, data) => {
-    const { i18n } = this.props;
-    const path = window.location.href;
-    const newLanguage = data.value;
-
-    i18n.changeLanguage(newLanguage)
-
-    // Handle switching between blog posts
-    if (path.includes('blog')) {
-      const blogSlug = path.split('blog/')[1]
-      navigate(`${newLanguage}/blog/${blogSlug}`);
-    }
+    // const { i18n } = this.props;
+    // const path = window.location.href;
+    // const newLanguage = data.value;
+    //
+    // i18n.changeLanguage(newLanguage)
+    //
+    // // Handle switching between blog posts
+    // if (path.includes('blog')) {
+    //   const blogSlug = path.split('blog/')[1]
+    //   navigate(`${newLanguage}/blog/${blogSlug}`);
+    // }
   }
 
   render() {
-    const { i18n, t } = this.props;
-
-    const cleanedUpLocaleName = i18n.language.split('-')[0];
+    // const cleanedUpLocaleName = i18n.language.split('-')[0];
+    const cleanedUpLocaleName = 'en';
 
     const languageOptions = [
-      { key: "en", text: t('shared:english'), value: "en" },
-      { key: "fr", text: t('shared:french'), value: "fr" },
+      { key: "en", text: 'shared:english', value: "en" },
+      { key: "fr", text: 'shared:french', value: "fr" },
     ]
 
     return (
@@ -50,4 +48,4 @@ class FooterInfoLanguageSwitcher extends Component {
   }
 }
 
-export default translate('layout')(FooterInfoLanguageSwitcher);
+export default FooterInfoLanguageSwitcher;
