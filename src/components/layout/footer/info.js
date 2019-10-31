543d732c03fe863d9c589e84095284135b55913a
dafuga <danielfugere28@gmail.com>
2019-10-31 20:07:46 +0900

    polishing: fixed contact part of footer

--- src/components/layout/footer/info.js
+++ src/components/layout/footer/info.js
@@ -11,26 +11,6 @@ import footerInfoStyles from './info.module.css';
 
 class FooterInfo extends Component {
   render() {
-    // TODO: Responsive logic needs reworking
-    // <Responsive {...Responsive.onlyMobile}>
-    //   <FooterInfoContact />
-    //   <Divider />
-    //   <FooterInfoResources />
-    // </Responsive>
-    // <Responsive minWidth={Responsive.onlyTablet.minWidth}>
-    //   <Grid container>
-    //     <Grid.Column
-    //       className={footerInfoStyles.contactInfoContainer}
-    //       width={10}
-    //     >
-    //       <FooterInfoResources />
-    //     </Grid.Column>
-    //
-    //     <Grid.Column tablet={6} computer={5} floated="right">
-    //       <FooterInfoContact />
-    //     </Grid.Column>
-    //   </Grid>
-    // </Responsive>
     return (
       <div className={footerInfoStyles.container}>
         <Grid container className={footerInfoStyles.container}>
