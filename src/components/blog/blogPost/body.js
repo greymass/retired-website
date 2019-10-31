2e14f867f9c9346ab282d6268ae20fdf01b2c053
dafuga <danielfugere28@gmail.com>
2019-10-31 20:15:51 +0900

    fix: fixed markdown container on mobile

--- src/components/blog/blogPost/body.js
+++ src/components/blog/blogPost/body.js
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
 
-import { Container, Responsive } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 
 import BlogPostDate from './body/date';
-import BlogPostDesktop from './body/desktop'
+import BlogPostContent from './body/content'
 
-import blogPostStyles from './body.module.css';
+import blogPostBodyStyles from './body.module.css';
 
 class BlogPostBody extends Component {
   render() {
     const { post } = this.props;
-    // TODO: Responsive logic needs reworking
-    // <Responsive maxWidth={Responsive.onlyTablet.maxWidth}>
-    //   <div dangerouslySetInnerHTML={{ __html: post.html }} />
-    // </Responsive>
-    // <Responsive minWidth={Responsive.onlyComputer.minWidth}>
-    //   <BlogPostDesktop post={post} />
-    // </Responsive>
+
     return (
-      <Container className={blogPostStyles.markdownContainer}>
+      <Container className={blogPostBodyStyles.markdownContainer}>
         <BlogPostDate post={post} />
-        <BlogPostDesktop post={post} />
+        <BlogPostContent post={post} />
       </Container>
     )
   }
