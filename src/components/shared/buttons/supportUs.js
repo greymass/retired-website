50b01b567b2e1cb23c70f51d30c27de0e57c6f75
dafuga <danielfugere28@gmail.com>
2019-11-09 17:54:44 +0900

    fix: fixed styling of shared support button

--- src/components/shared/buttons/supportUs.js
+++ src/components/shared/buttons/supportUs.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import { injectIntl } from "gatsby-plugin-intl";
 import { Link } from 'gatsby';
 
-import supportUsButtonStyles from './supportUs.module.css';
+import supportUsStyles from './supportUs.module.css';
 
 class SupportUsButton extends Component {
   render() {
     const { intl } = this.props;
     return (
       <Link to={`/${intl.locale}/support`}>
-        <div className={supportUsButtonStyles.supportUsButton}>
-          <h3 style={supportUsButtonStyles.supportUsText}>
+        <div className={supportUsStyles.supportUsButton}>
+          <h3 className={supportUsStyles.supportUsText}>
             {intl.formatMessage({ id: 'shared_support_us' })}
           </h3>
         </div>
