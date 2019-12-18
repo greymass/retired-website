996aa83b6beda3118834185cc7d1c3d5ee54a509
Aaron Cox <aaron@greymass.com>
2019-12-17 21:48:25 -0500

    Bit of design work on the APIs page

--- src/components/shared/sections/apis/table.js
+++ src/components/shared/sections/apis/table.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 import { injectIntl } from 'gatsby-plugin-intl';
 
-import { Table, Header, Image, Label, Icon } from "semantic-ui-react"
+import { Table, Header, Image, Label, Icon, Responsive } from "semantic-ui-react"
 
-import EosApi from 'eosjs-api';
+import { JsonRpc } from 'eosjs';
 
 import eosLogo from '../../../../images/blockchain-eos-logo.svg';
 import instarLogo from '../../../../images/blockchain-insights-logo.svg';
@@ -18,112 +18,136 @@ import apisTableStyles from './table.module.css';
 const nodes = [
   {
     url: 'https://eos.greymass.com',
-    name: 'eos',
+    name: 'EOS',
     logo: eosLogo,
     v1: true,
   },
   {
-    url: 'https://telos.greymass.com',
-    name: 'telos',
-    logo: telosLogo,
-    v1: true,
-  },
-  {
-    url: 'https://jungle.greymass.com',
-    name: 'jungle',
-    logo: jungleLogo,
-    v1: true,
-  },
-  {
-    url: 'https://wax.greymass.com',
-    name: 'wax',
-    logo: waxLogo,
-    v1: true,
-  },
-  {
     url: 'https://instar.greymass.com',
-    name: 'instar',
+    name: 'Instar',
     logo: instarLogo,
     v1: true,
   },
   {
     url: 'https://lynx.greymass.com',
-    name: 'lynx',
+    name: 'Lynx',
     logo: lynxLogo,
+    v1: true,
+  },
+  {
+    url: 'https://telos.greymass.com',
+    name: 'Telos',
+    logo: telosLogo,
+    v1: true,
+  },
+  {
+    url: 'https://wax.greymass.com',
+    name: 'WAX',
+    logo: waxLogo,
+    v1: true,
+  },
+  {
+    url: 'https://jungle.greymass.com',
+    name: 'Jungle (Testnet)',
+    logo: jungleLogo,
     v1: true,
   },
 ]
 
 class Apis extends React.Component {
-  state = { responseTimes: {}, triggeredUpdate: false };
+  state = {
+    responses: [],
+  };
 
   componentDidMount() {
-    const isSSR = typeof window === 'undefined';
-
-    if (isSSR) {
+    if (typeof window === 'undefined') {
       return;
     }
-
-    setInterval(() => this.tick(), 30000);
-
     this.tick();
+    setInterval(() => this.tick(), 3000);
   }
 
-  tick = async () => {
-    const newResponseTimes = {};
-
-    await Promise.all(nodes.map(node => {
-      return new Promise(resolve => {
-        checkResponseTime(node).then(responseTime => {
-          newResponseTimes[node.name] = responseTime;
-
-          resolve();
-        })
-      })
+  tick = async () => this.setState({
+    responses: await Promise.all(nodes.map(async (node) => {
+      try {
+        const start = Date.now();
+        const eos = new JsonRpc(node.url);
+        const info = await eos.get_info()
+        return {
+          ...node,
+          height: info.head_block_num,
+          ms: Date.now() - start,
+          time: info.head_block_time,
+        }
+      } catch (err) {
+        return {
+          ...node,
+          err,
+        }
+      }
     }))
-
-    this.setState({ responseTimes: newResponseTimes });
-  }
+  })
 
   render() {
     const { intl } = this.props;
+    const { responses } = this.state;
 
-    const { responseTimes } = this.state;
+    // wait for first responses
+    if (!responses.length) return false
 
     return (
       <div className={apisTableStyles.container}>
-        <Table unstackable>
+        <Table definition unstackable>
           <Table.Header>
             <Table.Row>
-              <Table.HeaderCell>{intl.formatMessage({ id: 'shared_apis_table_header_name' })}</Table.HeaderCell>
-              <Table.HeaderCell>{intl.formatMessage({ id: 'shared_apis_table_header_status' })}</Table.HeaderCell>
-              <Table.HeaderCell>{intl.formatMessage({ id: 'shared_apis_table_header_response_time' })}</Table.HeaderCell>
+              <Table.HeaderCell />
+              <Table.HeaderCell>
+                {intl.formatMessage({ id: 'shared_apis_table_header_name' })}
+              </Table.HeaderCell>
+              <Table.HeaderCell textAlign="center">
+                {intl.formatMessage({ id: 'shared_apis_table_header_features' })}
+              </Table.HeaderCell>
+              <Responsive as={Table.HeaderCell} minWidth={768} textAlign="center">
+                {intl.formatMessage({ id: 'shared_apis_table_header_height' })}
+              </Responsive>
+              <Responsive as={Table.HeaderCell} minWidth={768} textAlign="center">
+                {intl.formatMessage({ id: 'shared_apis_table_header_status' })}
+              </Responsive>
+              <Table.HeaderCell textAlign="center">
+                {intl.formatMessage({ id: 'shared_apis_table_header_response_time' })}
+              </Table.HeaderCell>
             </Table.Row>
           </Table.Header>
-
           <Table.Body>
-            {nodes.map(node =>  (
-              <Table.Row key={node.name}>
+            {responses.map(response =>  (
+              <Table.Row key={response.name}>
+                <Table.Cell collapsing>
+                  <Image src={response.logo} rounded size='mini'/>
+                </Table.Cell>
                 <Table.Cell>
                   <Header as='h4' image>
-                    <Image src={node.logo} rounded size='mini'/>
                     <Header.Content>
-                      {node.name}
-                      <Header.Subheader>{node.url}</Header.Subheader>
-                      {node.v1 && (<Label size="mini" color="teal" content="v1 history"/>)}
+                      {response.name}
+                      <Header.Subheader>{response.url}</Header.Subheader>
                     </Header.Content>
                   </Header>
                 </Table.Cell>
-                <Table.Cell>
-                  {!!responseTimes[node.name] ? (
+                <Responsive as={Table.Cell} minWidth={768} textAlign="center">
+                  <Label color="blue" content="History (V1)"/>
+                </Responsive>
+                <Responsive as={Table.Cell} minWidth={768} textAlign="center">
+                  {response.height}
+                </Responsive>
+                <Table.Cell textAlign="center">
+                  {!!response.ms ? (
                     <Icon name="check" size="large" color="green" />
                   ) : (
                     <Icon name="close" size="large" color="red" />
                   )}
                 </Table.Cell>
-                <Table.Cell>
-                  {responseTimes[node.name] ?
-                    `${responseTimes[node.name]} ms` :
+                <Table.Cell textAlign="center">
+                  {response.ms ?
+                    `${response.ms} ms` :
                     '----'
                   }
                 </Table.Cell>
@@ -137,16 +161,3 @@ class Apis extends React.Component {
 }
 
 export default injectIntl(Apis);
-
-async function checkResponseTime(node) {
-  const eos = EosApi({ httpEndpoint:  node.url });
-  const timeBefore = Date.now();
-
-  const fetchedInfo = await new Promise(resolve => {
-    eos.getInfo((error) => {
-      resolve(!error);
-    });
-  });
-
-  return fetchedInfo && Date.now() - timeBefore;
-}
