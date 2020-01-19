9c817c951e91ab70dbecbed348ebac86c22c833f
dafuga <danielfugere28@gmail.com>
2020-01-19 20:37:22 +0900

    fix: got fuel scatter login in working state

--- src/components/shared/wrappers/transit.js
+++ src/components/shared/wrappers/transit.js
@@ -1,34 +1,35 @@
 import React from 'react';
-import { injectIntl } from "gatsby-plugin-intl";
 
 import { Link } from 'anchor-link';
 
 import { initAccessContext } from 'eos-transit';
 import scatter from 'eos-transit-scatter-provider';
 
-import { Api, JsonRpc } from 'eosjs';
-
 const chainId = 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906';
-const apiNode = 'https://eos.greymass.com';
+const apiNode = 'eos.greymass.com';
 
 class TransitWrapper extends React.Component {
-  async constructor(props) {
+  constructor(props) {
     super(props);
 
-    await this.initTransit();
-    this.initAnchor();
     this.state = {
       account: false,
       signer: false,
       tx: false,
+      wallet: false,
     };
   }
+
+  async componentDidMount() {
+    await this.initTransit();
+    this.initAnchor();
+  }
+
   initTransit = async () => {
     const accessContext = initAccessContext({
-      appName: 'my_first_dapp',
+      appName: 'greymass.com',
       network: {
         host: apiNode,
-        port: 443,
         protocol: 'https',
         chainId
       },
@@ -43,11 +44,7 @@ class TransitWrapper extends React.Component {
 
     const wallet = accessContext.initWallet(selectedProvider);
 
-    await wallet.connect();
-
-    console.log({discoveryData});
-
-    this.setState({wallet} );
+    this.setState({ wallet } );
   }
   initAnchor = () => {
     const t = this;
@@ -87,37 +84,41 @@ class TransitWrapper extends React.Component {
       transport: WebLinkTransport,
     });
   }
-  setSigner = (signer) => {
+  setSigner = async (signer) => {
     const { wallet } = this.state;
-    this.setState({ signer }, async () => {
-      switch(signer) {
-        case "scatter": {
-          const discoveryData = await wallet.discover();
 
-          if (discoveryData.keyToAccountMap.length > 0) {
-            await wallet.login(accountName, authorization)
-          } else {
-            await wallet.login();
+    this.setState({ signer });
+
+    switch(signer) {
+      case "scatter": {
+        await wallet.connect();
+        const response = await wallet.login();
+        console.log({response})
+        const { account_name, permissions } = response;
+        this.setState({
+          account: {
+            name: account_name,
+            authority: permissions[0] && permissions[0].perm_name
           }
-          break;
-        }
-        case "anchor": {
-          const identityRequest = await this.link.createRequest({
-            identity: {permission: undefined},
-            info: undefined,
-          })
-          this.setState({
-            identityRequest
-          })
-          const identity = await this.link.sendRequest(identityRequest)
-          break;
-        }
-        default: {
-          // reset
-          clearInterval(this.detect)
-        }
+        });
+
+        break;
       }
-    })
+      case "anchor": {
+        const identityRequest = await this.link.createRequest({
+          identity: {permission: undefined},
+          info: undefined,
+        })
+        this.setState({
+          identityRequest
+        })
+        await this.link.sendRequest(identityRequest)
+        break;
+      }
+      default: {
+        break;
+      }
+    }
   }
   clearTx = () => this.setState({ tx: false })
   transact = async (transaction, config) => {
