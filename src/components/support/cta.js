34407f607a0c4b41f462b9abb71f418a2de6e16e
dafuga <danielfugere28@gmail.com>
2020-01-25 14:34:55 +0900

    enhancement: allowing user to vote for greymass directly

--- src/components/support/cta.js
+++ src/components/support/cta.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import TransactionHandlersProxy from './transactionHandlers/proxy';
+import TransactionHandlersVoteproducer from './transactionHandlers/voteproducer';
 import { Container } from 'semantic-ui-react';
 
 class SupportCTA extends Component {
   render() {
     return (
      <Container style={{ minHeight: '1000px', padding: '50px 0' }}>
-       <TransactionHandlersProxy />
+       <TransactionHandlersVoteproducer />
      </Container>
     )
   }
