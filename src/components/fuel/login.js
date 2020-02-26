51de5bdd55278b001340243a04a9b9b9ecb0c55f
dafuga <danielfugere28@gmail.com>
2020-02-26 02:16:05 -0800

    chore: integrated new transit/login component on fuel page

--- src/components/fuel/login.js
+++ src/components/fuel/login.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Button, Header, Grid, Segment } from 'semantic-ui-react';
+import { Header, Grid, Segment } from 'semantic-ui-react';
+import TransitLogin from '../shared/sections/transit/login';
 
 class FuelLogin extends Component {
   render() {
@@ -12,18 +13,11 @@ class FuelLogin extends Component {
               <Header size="large">
                 EOSIO Network Resources On-Demand
                 <Header.Subheader>
-                  Login with either Anchor or Scatter to get started!
+                  Select a Blockchain to get started!
                 </Header.Subheader>
               </Header>
-              <Button
-                content="Login with Anchor"
-                primary
-                onClick={() => setSigner('anchor-link')}
-              />
-              <Button
-                content="Login with Scatter"
-                primary
-                onClick={() => setSigner('scatter')}
+              <TransitLogin
+                setSigner={setSigner}
               />
             </Segment>
           </Grid.Column>
