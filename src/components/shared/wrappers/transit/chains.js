f0399bf82091295fd939a812efde58d48e5ec0fd
dafuga <danielfugere28@gmail.com>
2020-03-01 15:24:23 +0900

    refactor: refactored the transit wrapper

---
+++ src/components/shared/wrappers/transit/chains.js
@@ -0,0 +1,9 @@
+const chains = {
+  eos: {
+    name: 'EOS',
+    chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
+    apiNode: 'eos.greymass.com',
+  },
+}
+
+export default chains;
