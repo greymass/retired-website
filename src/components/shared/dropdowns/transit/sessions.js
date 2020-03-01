cc5c3087807d25d0729eac5abcd2b193fa8840c7
dafuga <danielfugere28@gmail.com>
2020-03-01 15:59:41 +0900

    chore: added a transit dropdown component to project

--- src/components/shared/dropdowns/transit/sessions.js
+++ src/components/shared/dropdowns/transit/sessions.js
@@ -1,67 +1,60 @@
 import React  from 'react';
-import { Button, Header, Segment, Grid, Dropdown, Modal, Divider, Container } from "semantic-ui-react"
+import { Dropdown } from 'semantic-ui-react';
 
-import { debounce } from 'lodash';
+import TransitWrapper from '../../../shared/wrappers/transit';
 
-import TransitWrapper from '../../shared/wrappers/transit';
+import SharedModalsTransitLogin from '../../../shared/modals/transit';
+
 import { Card } from "semantic-ui-react/dist/commonjs/views/Card"
 import { List } from "semantic-ui-react/dist/commonjs/elements/List"
 import { Form } from "semantic-ui-react/dist/commonjs/collections/Form"
 
-class SharedModalsTransitLogin extends TransitWrapper {
+class SharedDropdownsTransitLogin extends TransitWrapper {
   state = {
-    processing: false,
+    loggingIn: false,
+    transitSessions: [],
   };
-
-  componentDidMount() {
-    const { blockchain } = this.props;
-
-    this.setState({ blockchain });
-  }
-
-  transitLogin = async (walletName) => {
-    const { setSigner } = this.props;
-    const { blockchain } = this.state;
-
-    this.setState({
-      processing: true,
-    })
-    await setSigner(walletName, blockchain);
-    this.setState({ processing: false });
-  }
 
   render() {
     const {
-      onClose,
-    } = this.props;
-    const {
-      account,
-      blockchain,
-      processing,
+      loggingIn,
+      transitSessions,
     } = this.state;
 
     return (
-      <Dropdown text='Shopping' pointing className='link item'>
-        <Dropdown.Menu>
-          <Dropdown.Header>Categories</Dropdown.Header>
-          <Dropdown.Item>
-            <Dropdown text='Switch Account'>
-              <Dropdown.Menu>
-                <Dropdown.Header>Select Account</Dropdown.Header>
-                {transitSessions.map(transitSession => (
-                  <Dropdown.Item>{transitSession.name}</Dropdown.Item>
-                ))}
-                <Dropdown.Item>Shirts</Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Header>+ Add account</Dropdown.Header>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Dropdown.Item>
-          <Dropdown.Item>Login</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
+      <>
+        <Dropdown text='Shopping' pointing className='link item'>
+          <Dropdown.Menu>
+            <Dropdown.Header>Switch Accounts</Dropdown.Header>
+            <Dropdown.Item>
+              <Dropdown text='Switch Account'>
+                <Dropdown.Menu>
+                  <Dropdown.Header>Select Account</Dropdown.Header>
+                  {transitSessions.map(transitSession => (
+                    <Dropdown.Item>{transitSession.name}</Dropdown.Item>
+                  ))}
+                  <Dropdown.Divider />
+                  <Dropdown.Header>
+                    <a onClick={() => this.setState({ loggingIn: true })}>
+                      + Add account
+                    </a>
+                  </Dropdown.Header>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Dropdown.Item>
+            <Dropdown.Item>
+              <a onClick={this.logout}>
+                Logout
+              </a>
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        {loggingIn && (
+          <SharedModalsTransitLogin />
+        )}
+      </>
     );
   }
 }
 
-export default SharedModalsTransitLogin;
+export default SharedDropdownsTransitLogin;
