c9fc410f7e6c84d9f3aebf7fa41a134b8d1bda6a
dafuga <danielfugere28@gmail.com>
2020-03-08 15:00:39 +0900

    enhancement: little tweaks to the shared header

--- src/components/shared/sections/header.js
+++ src/components/shared/sections/header.js
@@ -22,7 +22,7 @@ export default class SharedHeader extends Component {
                 paragraph ? sharedHeaderStyles.longBlueBar :  ''
               }`}/>
             </Grid.Column>
-            <Grid.Column computer={8} tablet={8} mobile={12}>
+            <Grid.Column computer={10} tablet={8} mobile={12}>
               <h4 className={sharedHeaderStyles.headerText}>
                 {title}
               </h4>
