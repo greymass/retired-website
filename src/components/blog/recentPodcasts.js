d80b2f0e94e4c8a752aeb25e781b8bf40353923a
dafuga <danielfugere28@gmail.com>
2020-03-08 19:45:10 +0900

    enhancement: added podcasts page

--- src/components/blog/recentPodcasts.js
+++ src/components/blog/recentPodcasts.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Header, Icon, Image, List } from 'semantic-ui-react';
-import { graphql, StaticQuery } from 'gatsby';
+import { graphql, Link, StaticQuery } from 'gatsby';
 import { injectIntl } from 'gatsby-plugin-intl';
+
+import SharedListItemsPodcast from '../shared/list/items/podcast';
 
 import podcastLogo from '../../images/podcast.jpg'
 import recentPodcastsStyles from './recentPodcasts.module.css';
@@ -12,7 +14,6 @@ class BlogRecentPodcasts extends Component {
 
     return (
       <div className={recentPodcastsStyles.container}>
-
         <Image rounded src={podcastLogo} />
         <Header
           as="h3"
@@ -20,20 +21,18 @@ class BlogRecentPodcasts extends Component {
           content={intl.formatMessage({ id: 'blog_recent_podcasts_header' })}
         />
         <List>
-          {data.podcasts.edges.map(({ node: podcast }) => {
-            return (
-              <Podcast
-                title={intl.formatMessage({ id: `podcast_${podcast.key}_title` })}
-                date={new Date(podcast.date).toLocaleDateString()}
-                link={podcast.link}
-              />
-            )
-            })}
+          {data.podcasts.edges.map(({ node: podcast }) => (
+            <SharedListItemsPodcast
+              title={intl.formatMessage({ id: `podcast_${podcast.key}_title` })}
+              date={podcast.date}
+              link={podcast.link}
+            />
+          ))}
         </List>
-        <a href="/#podcasts" className={recentPodcastsStyles.link}>
+        <Link to={`/${intl.locale}/podcasts`} className={recentPodcastsStyles.link}>
           {intl.formatMessage({ id: 'blog_recent_podcasts_link' })}
           <Icon name="arrow right" className={recentPodcastsStyles.linkIcon} />
-        </a>
+        </Link>
       </div>
     );
   }
@@ -60,19 +59,3 @@ export default props => (
     render={data => <BlogRecentPodcastsWrapper data={data} {...props} />}
   />
 );
-
-const Podcast = ({ title, date, link }) => {
-  return (
-    <a href={link}>
-      <div className={recentPodcastsStyles.podcastContainer}>
-        <h3 className={recentPodcastsStyles.podcastHeader}>
-          {title}
-        </h3>
-        <h4 className={recentPodcastsStyles.podcastDate}>
-          <Icon name="calendar alternate outline" />
-          {date}
-        </h4>
-      </div>
-    </a>
-  );
-}
