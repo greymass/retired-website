8cb2bb18fafade4db332087d7c936e5b86350ff7
Aaron Cox <aaron@greymass.com>
2020-03-11 16:59:23 -0400

    Added more chain logos

--- src/components/shared/elements/chainLogo.js
+++ src/components/shared/elements/chainLogo.js
@@ -6,13 +6,19 @@ import {
 import chains from '../../../constants/chains';
 
 import eosLogo from '../../../images/blockchains/eos.png';
+import fioLogo from '../../../images/blockchains/fio.svg';
+import instarLogo from '../../../images/blockchains/instar.svg';
 import jungleLogo from '../../../images/blockchains/jungle.png';
+import lynxLogo from '../../../images/blockchains/lynx.svg'
 import telosLogo from '../../../images/blockchains/telos.png';
 import waxLogo from '../../../images/blockchains/wax.png';
 
 const logos = {
   'eos': eosLogo,
+  'fio': fioLogo,
+  'instar': instarLogo,
   'jungle': jungleLogo,
+  'lynx': lynxLogo,
   'telos': telosLogo,
   'wax': waxLogo,
 }
