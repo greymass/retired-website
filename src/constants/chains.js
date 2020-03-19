a519629d9170bffbe2dec79c689d0952cecacd13
dafuga <danielfugere28@gmail.com>
2020-03-19 21:08:26 +0900

    enhancement: only enabling fuel on chains that support it

--- src/constants/chains.js
+++ src/constants/chains.js
@@ -3,21 +3,25 @@ const chains = {
     name: 'eos',
     chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
     apiNode: 'eos.greymass.com',
+    fuel: true,
   },
   wax: {
     name: 'wax',
     chainId: '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4',
-    apiNode: 'wax.greymass.com'
+    apiNode: 'wax.greymass.com',
+    fuel: false,
   },
   telos: {
     name: 'telos',
     chainId: '4667b205c6838ef70ff7988f6e8257e8be0e1284a2f59699054a018f743b1d11',
     apiNode: 'telos.greymass.com',
+    fuel: false,
   },
   jungle: {
     name: 'jungle',
     chainId: 'e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473',
-    apiNode: 'jungle.greymass.com'
+    apiNode: 'jungle.greymass.com',
+    fuel: true,
   }
 }
 
