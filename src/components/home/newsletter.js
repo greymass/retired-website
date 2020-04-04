4b5ca84d4f265161db5fd3638dbaba77e403c235
dafuga <danielfugere28@gmail.com>
2020-04-04 18:27:52 +0900

    addition: added message about how to contribute to newsletter on homepage

--- src/components/home/newsletter.js
+++ src/components/home/newsletter.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { injectIntl } from "gatsby-plugin-intl";
+import { injectIntl, FormattedHTMLMessage } from 'gatsby-plugin-intl';
 import { Container } from 'semantic-ui-react';
 
 import newsletterStyles from './newsletter.module.css';
@@ -7,13 +7,14 @@ import newsletterStyles from './newsletter.module.css';
 class HomeNewsletter extends Component {
   render() {
     const { intl } = this.props;
+
     return (
       <Container className={newsletterStyles.container}>
         <p className={newsletterStyles.title}>
           {intl.formatMessage({ id: 'home_newsletter_title' })}
         </p>
         <p className={newsletterStyles.subtitle}>
-          {intl.formatMessage({ id: 'home_newsletter_subtitle' })}
+          <FormattedHTMLMessage id="home_newsletter_subtitle" />
         </p>
         <iframe
           title="newsletter-iframe"
