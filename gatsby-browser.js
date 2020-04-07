35566be94fdb8e079d007168c43b1ffbfbd7782c
Aaron Cox <aaron@greymass.com>
2020-04-07 19:11:56 -0400

    Fixed #61

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,20 @@
-import "./src/styles/global.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+require("./src/styles/global.css");
+require("slick-carousel/slick/slick.css");
+require("slick-carousel/slick/slick-theme.css");
+
+exports.shouldUpdateScroll = ({
+  routerProps: { location },
+  getSavedScrollPosition,
+}) => {
+  const { pathname } = location
+
+  // List of routes to not scroll to top
+  const scrollIgnoreRoutes = []
+
+  // If the pathname is not included, scroll to top
+  if (scrollIgnoreRoutes.indexOf(pathname) === -1) {
+    window.scrollTo(0, 0)
+  }
+
+  return false
+}
