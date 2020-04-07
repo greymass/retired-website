c471a148b814bf42e97ff75126a8228287db9c6a
Aaron Cox <aaron@greymass.com>
2020-04-07 14:02:03 -0400

    Added gatsby-remark-vscode

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,9 +80,24 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
+          {
+            resolve: `gatsby-remark-vscode`,
+            options: {
+              theme: 'Dark (Visual Studio)', // Read on for list of included themes. Also accepts object and function forms.
+              wrapperClassName: '',   // Additional class put on 'pre' tag. Also accepts function to set the class dynamically.
+              injectStyles: true,     // Injects (minimal) additional CSS for layout and scrolling
+              extensions: [],         // Third-party extensions providing additional themes and languages
+              languageAliases: {},    // Map of custom/unknown language codes to standard/known language codes
+              replaceColor: x => x,   // Function allowing replacement of a theme color with another. Useful for replacing hex colors with CSS variables.
+              getLineClassName: ({    // Function allowing dynamic setting of additional class names on individual lines
+                content,              //   - the string content of the line
+                index,                //   - the zero-based index of the line within the code fence
+                language,             //   - the language specified for the code fence
+                meta                  //   - any options set on the code fence alongside the language (more on this later)
+              }) => '',
+              logLevel: 'info'       // Set to 'info' to debug if something looks wrong
+            }
+          }
         ],
       },
     },
