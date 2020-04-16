46f13b661498011eacba148984c135abf3262469
dafuga <danielfugere28@gmail.com>
2020-04-17 07:54:33 +0900

    revert: removed second favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,8 +9,6 @@ import LayoutFooter from './layout/footer';
 
 import layoutStyles from './layout.module.css';
 
-import favicon from '../../static/favicon.ico';
-
 class Layout extends Component {
   render() {
     const {
@@ -30,7 +28,6 @@ class Layout extends Component {
         <Helmet>
           <meta charSet="utf-8" />
           <title>{data.site.siteMetadata.title}</title>
-          <link rel="shortcut icon" type="image/png" href={favicon} />
           <link rel="canonical" href="https://greymass.com" />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" integrity="sha256-9mbkOfVho3ZPXfM7W8sV2SndrGDuh7wuyLjtsWeTI1Q=" crossorigin="anonymous" />
         </Helmet>
