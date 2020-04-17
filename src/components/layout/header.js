9eed579ff091a8e1877714e738a228de8375b0f7
dafuga <danielfugere28@gmail.com>
2020-04-17 10:00:47 +0900

    chore: added missing french translations on mobile view

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -46,7 +46,7 @@ class Header extends TransitWrapper {
     const navbarItems = [
       {
         as: `/${intl.locale}/projects`,
-        content: 'projects',
+        content: intl.formatMessage({ id: 'layout_projects' }),
         dropdown: (
           <Menu
             secondary
@@ -57,32 +57,32 @@ class Header extends TransitWrapper {
               to={`/${intl.locale}/anchor`}
               style={{ display: 'none' }}
             >
-              Anchor Wallet
+              {intl.formatMessage({ id: 'layout_anchor_wallet' })}
             </Menu.Item>
             <Menu.Item
               as={Link}
               to={`/${intl.locale}/esr`}
               style={{ display: 'none' }}
             >
-              ESR Protocol
+              {intl.formatMessage({ id: 'layout_esr_protocol' })}
             </Menu.Item>
             <Menu.Item
               as={Link}
               to={`/${intl.locale}/fuel`}
             >
-              Fuel
+              {intl.formatMessage({ id: 'layout_fuel' })}
             </Menu.Item>
             <Menu.Item
               as={Link}
               to={`/${intl.locale}/apis`}
             >
-              Public APIs
+              {intl.formatMessage({ id: 'layout_public_apis' })}
             </Menu.Item>
             <Menu.Item
               as={Link}
               to={`/${intl.locale}/projects`}
             >
-              View all project
+              {intl.formatMessage({ id: 'layout_view_all_projects' })}
             </Menu.Item>
           </Menu>
         ),
@@ -90,7 +90,7 @@ class Header extends TransitWrapper {
       },
       {
         as: `/${intl.locale}/about`,
-        content: 'about',
+        content: intl.formatMessage({ id: 'layout_about' }),
         key: 'about'
       },
     ];
@@ -99,7 +99,7 @@ class Header extends TransitWrapper {
       navbarItems.push(
         {
           as: `/${intl.locale}/blog/1`,
-          content: 'blog',
+          content: intl.formatMessage({ id: 'layout_blog' }),
           key: 'blog'
         }
       );
