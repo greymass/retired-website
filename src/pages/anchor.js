cf96551af6c81048838dbb3cc526c98bfe6d891d
Aaron Cox <aaron@greymass.com>
2020-09-11 14:08:36 -0400

    Updated Anchor to 1.1.2

--- src/pages/anchor.js
+++ src/pages/anchor.js
@@ -33,8 +33,8 @@ class Anchor extends TransitWrapper {
       location,
       intl,
     } = this.props;
-    const version = "1.1.1";
-    const release = "2020/09/10"
+    const version = "1.1.2";
+    const release = "2020/09/11"
     return (
       <Layout location={location}>
         <SEO
