6ab5a05b9ac342ca5f72c9897eafa947b4eb10cf
Aaron Cox <aaron@greymass.com>
2021-09-17 12:13:47 -0400

    Anchor 1.2.9

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,14 +18,14 @@ module.exports = {
       youtube: 'https://www.youtube.com/channel/UCKUIgioqwxJhZFUTdSPCAGA',
     },
     anchor: {
-      desktopReleaseDate: '2021/08/18',
-      desktopVersion: '1.2.6',
+      desktopReleaseDate: '2021/09/18',
+      desktopVersion: '1.2.9',
       iosDownloadUrl: 'https://apps.apple.com/us/app/anchor-wallet/id1487410877',
       iosReleaseDate: '2021/05/04',
       iosVersion: '1.2.1',
       linuxDownloadUrl: 'https://github.com/greymass/anchor/releases',
-      macDownloadUrl: 'https://github.com/greymass/anchor/releases/download/v1.2.6/mac-anchor-wallet-1.2.6.dmg',
-      windowsDownloadUrl: 'https://github.com/greymass/anchor/releases/download/v1.2.6/win-anchor-wallet-1.2.6.exe',
+      macDownloadUrl: 'https://github.com/greymass/anchor/releases/download/v1.2.9/mac-anchor-wallet-1.2.9-x64.dmg',
+      windowsDownloadUrl: 'https://github.com/greymass/anchor/releases/download/v1.2.9/win-anchor-wallet-1.2.9.exe',
     }
   },
   plugins: [
