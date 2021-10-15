259c8b090e27b793449dccce31556b5e3a73d6f3
dafuga <danielfugere28@gmail.com>
2021-10-14 22:38:03 -0700

    chore: adding explanation overlay to backup sheet example

--- src/pages/backups.js
+++ src/pages/backups.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 import { injectIntl } from "gatsby-plugin-intl";
 
-import { Image, Grid } from "semantic-ui-react";
+import { Image } from "semantic-ui-react";
 
 import Layout from '../components/layout';
 import SEO from '../components/shared/seo';
 
-import IOSInstructions from '../components/backups/instructions/IOS';
-import AndroidInstructions from '../components/backups/instructions/Android';
-import DesktopInstructions from '../components/backups/instructions/Desktop';
+import Instructions from '../components/backups/Instructions';
+import Explanation from '../components/backups/Explanation';
 
 import backupsHeader from "../images/backups_header.jpg";
-import keyCertificate from "../images/key_certificate.png";
 
 import backupStyles from './backups.module.css';
-
-const platforms = ['iOS', 'Android', 'Win/OSX/Linux']
 
 class Backups extends Component {
   state = {
@@ -43,70 +39,9 @@ class Backups extends Component {
           </div>
 
           <div className={backupStyles.innerContainer}>
-            <div className={backupStyles.steps}>
-              <h2 className={backupStyles.title}>Account recovery</h2>
-              <p>
-                Follow these steps to recover your account
-              </p>
-              <Grid>
-                <Grid.Column computer={13} tablet={16} mobile={16}>
-                  <strong>
-                    Choose your platform for recovery
-                  </strong>
-                  <div className={backupStyles.buttonsContainer}>
-                    {platforms.map(platform => (
-                      <button
-                        onClick={() => this.setState({currentPlatform: platform})}
-                        className={currentPlatform === platform ? backupStyles.active : backupStyles.inactive}
-                      >
-                        {platform}
-                      </button>
-                    ))}
-                  </div>
-
-                  {currentPlatform === 'iOS' && (
-                    <IOSInstructions />
-                  )}
-
-                  {currentPlatform === 'Android' && (
-                    <AndroidInstructions />
-                  )}
-
-                  {currentPlatform === 'Win/OSX/Linux' && (
-                    <DesktopInstructions />
-                  )}
-                </Grid.Column>
-                <Grid.Column computer={3} tablet={16} mobile={16}>
-                  <div className={backupStyles.warningBox}>
-                    <strong>
-                      Did you know?
-                    </strong>
-                    <br /><br />
-                    <p>
-                      This process can be used to setup your account on multiple devices securely or revoke all device keys if you have lost access to any devices
-                    </p>
-                  </div>
-                </Grid.Column>
-              </Grid>
-            </div>
+            <Instructions />
             <hr />
-            <div className={backupStyles.explanation}>
-              <h2 className={backupStyles.title}>Certificate explanation</h2>
-              <div className={backupStyles.mobileExplanation}>
-
-              </div>
-              <Image
-                alt='backups-header-image'
-                src={keyCertificate}
-                style={{ width: 400 }}
-              />
-              <div className={backupStyles.keyCertificateLayover}>
-
-              </div>
-              <div className={backupStyles.keyCertificateArrowExplanations}>
-
-              </div>
-            </div>
+            <Explanation />
             <div className={backupStyles.advanced}>
               <h2 className={backupStyles.title}>Advanced</h2>
 
