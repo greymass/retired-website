b5b3f9d8b58f975e40f6c45083b1767c27336eec
dafuga <danielfugere28@gmail.com>
2021-10-19 14:05:01 -0700

    chore: implemented new page header on account-recovery page

--- src/components/backups/Instructions.js
+++ src/components/backups/Instructions.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 
-import { Grid } from "semantic-ui-react";
+import { Grid, Image } from "semantic-ui-react";
 
 import IOSInstructions from "./instructions/IOS";
 import AndroidInstructions from "./instructions/Android";
 import DesktopInstructions from "./instructions/Desktop";
 
 import instructionsStyles from "./instructions.module.css";
+import backupStyles from "../../pages/account-recovery.module.css";
+import accountRecoveryHeaderImage from "../../images/account-recovery-header-image.jpg";
 
 const platforms = ['iOS', 'Android', 'Win/OSX/Linux']
 
@@ -18,52 +20,66 @@ export default class Instructions extends Component {
 
     return (
       <div className={instructionsStyles.container}>
-        <h2 className={instructionsStyles.title}>Account recovery</h2>
-        <p className={instructionsStyles.subtitle}>
-          Follow these steps to recover your account
-        </p>
-        <Grid>
-          <Grid.Column computer={13} tablet={16} mobile={16}>
-            <p className={instructionsStyles.subtitle}>
-              <strong >
-                Choose your platform for recovery
-              </strong>
-            </p>
-            <div className={instructionsStyles.buttonsContainer}>
-              {platforms.map(platform => (
-                <button
-                  onClick={() => this.setState({currentPlatform: platform})}
-                  className={currentPlatform === platform ? instructionsStyles.active : instructionsStyles.inactive}
-                >
-                  {platform}
-                </button>
-              ))}
-            </div>
-
-            {currentPlatform === 'iOS' && (
-              <IOSInstructions />
-            )}
-
-            {currentPlatform === 'Android' && (
-              <AndroidInstructions />
-            )}
-
-            {currentPlatform === 'Win/OSX/Linux' && (
-              <DesktopInstructions />
-            )}
+        <Grid className={`tablet reversed computer reversed ${backupStyles.header}`}>
+          <Grid.Column computer={6} tablet={6} mobile={16} floated="right">
+            <Image
+              alt='account-recovery-header-image'
+              src={accountRecoveryHeaderImage}
+              style={{ margin: 'auto', width: 400 }}
+            />
           </Grid.Column>
-          <Grid.Column computer={3} tablet={16} mobile={16}>
-            <div className={instructionsStyles.warningBox}>
-              <strong>
-                Did you know?
-              </strong>
-              <br /><br />
-              <p>
-                This process can be used to setup your account on multiple devices securely or revoke all device keys if you have lost access to any devices
+          <Grid.Column computer={8} tablet={8} mobile={16} floated="left">
+            <div className={instructionsStyles.headerTextContainer}>
+              <h2 className={instructionsStyles.title}>Account recovery</h2>
+              <p className={instructionsStyles.subtitle}>
+                Follow these steps to recover your account
               </p>
+              <p className={instructionsStyles.subtitle}>
+                <strong >
+                  Choose your platform for recovery
+                </strong>
+              </p>
+              <div className={instructionsStyles.buttonsContainer}>
+                {platforms.map(platform => (
+                  <button
+                    onClick={() => this.setState({currentPlatform: platform})}
+                    className={currentPlatform === platform ? instructionsStyles.active : instructionsStyles.inactive}
+                  >
+                    {platform}
+                  </button>
+                ))}
+              </div>
             </div>
           </Grid.Column>
         </Grid>
+        <div className={instructionsStyles.innerContainer}>
+          <Grid>
+            <Grid.Column computer={13} tablet={16} mobile={16}>
+              {currentPlatform === 'iOS' && (
+                <IOSInstructions />
+              )}
+
+              {currentPlatform === 'Android' && (
+                <AndroidInstructions />
+              )}
+
+              {currentPlatform === 'Win/OSX/Linux' && (
+                <DesktopInstructions />
+              )}
+            </Grid.Column>
+            <Grid.Column computer={3} tablet={16} mobile={16}>
+              <div className={instructionsStyles.warningBox}>
+                <strong>
+                  Did you know?
+                </strong>
+                <br /><br />
+                <p>
+                  This process can be used to setup your account on multiple devices securely or revoke all device keys if you have lost access to any devices
+                </p>
+              </div>
+            </Grid.Column>
+          </Grid>
+        </div>
       </div>
     );
   }
--- src/pages/account-recovery.js
+++ src/pages/account-recovery.js
@@ -1,42 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { injectIntl } from "gatsby-plugin-intl";
 
+import { Image } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
+
 import Layout from '../components/layout';
-import SharedHeader from '../components/shared/sections/header';
 import SEO from '../components/shared/seo';
 
-import { Container, Header, Segment } from 'semantic-ui-react';
+import Instructions from '../components/backups/Instructions';
+import Explanation from '../components/backups/Explanation';
 
-class AccountRecovery extends React.Component {
+import accountRecoveryHeaderImage from "../images/account-recovery-header-image.jpg";
+
+import backupStyles from './account-recovery.module.css';
+
+class AccountRecovery extends Component {
   render() {
     const { intl, location } = this.props;
 
     return (
-      <Layout location={location}>
+      <Layout containerClassName="anchor" location={location} >
         <SEO
           lang={intl.locale}
-          keywords={['account recovery', 'anchor', 'wallet']}
-          title="Account Recovery - Anchor Wallet"
+          keywords={['backups', 'greymass']}
+          title="Backups"
         />
-        <SharedHeader
-          title={intl.formatMessage({ id: 'account_recovery_header_title' })}
-          paragraph={intl.formatMessage({ id: 'account_recovery_header_paragraph' })}
-        />
-        <Container>
-          <Segment padded="very" size="large" style={{ margin: '3em 0' }}>
-            <Header size="huge" textAlign="center">
-              This feature and page are in development.
-            </Header>
-            <p>As part of our efforts to improve the EOSIO account creation process, we are creating a new account backup standard that will work within Anchor Wallet to securely backup the owner permission for any EOSIO account.</p> 
-            <p>For technical information about this encryption method, you are welcome to view the Swift prototype we have built. More information will be available as this feature is developed and released.</p>
-            <p>
-              <a href="https://github.com/greymass/swift-eosio-key-encryption">
-                https://github.com/greymass/swift-eosio-key-encryption
-              </a>
-            </p>
-          </Segment>
-        </Container>
+        <div className={backupStyles.container}>
+          <Instructions />
 
+          <div className={backupStyles.innerContainer}>
+            <hr />
+            <Explanation />
+            <hr />
+            <div className={backupStyles.advanced}>
+              <h2 className={backupStyles.title}>Advanced</h2>
+
+              <a href="https://github.com/greymass/keycert-js">
+                JS implementation of the keycert library
+              </a>
+
+              <a href="https://github.com/greymass/keycert-js-pdf">
+                Keycert PDF generation library
+              </a>
+            </div>
+          </div>
+        </div>
       </Layout>
     )
   }
